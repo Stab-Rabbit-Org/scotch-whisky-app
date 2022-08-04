6483e28ee24e97c63077d26ce897dcb56e3a9f6b
Rosend0 <83732379+Rosend0@users.noreply.github.com>
2022-08-04 14:36:00 -0600

    Rosendo/button (#6)
    * working post button
    
    * finished add button functionality
    
    * button

--- src/controllers/whiskeyController.js
+++ src/controllers/whiskeyController.js
@@ -35,6 +35,37 @@ whiskeyController.getWhiskeys = (req, res, next) => {
 
  };
 
+
+ whiskeyController.addWhiskeys = async (req, res, next) => {
+  try {
+
+    const {
+      id,
+      title,
+      img_url,
+      region,
+      price,
+      rating,
+      description,
+    } = req.body.whisky 
+    console.log('this is req.body.whisky.id', req.body.whisky.id)
+    console.log('WHISKEY CONTROLLER');
+    const values = [id, title, img_url, region, price, rating, description]
+    const addQuery = `INSERT INTO scotches(id,title,img_url,region,price,rating,description)
+                           VALUES ($1, $2, $3, $4, $5, $6, $7);` 
+    
+    const addWhiskeys = await db.query(addQuery, values)
+    res.locals.addWhiskey = addWhiskeys;
+     console.log('this is the added whiskey', req.body.whisky)
+    return next()
+  }
+  catch (err){
+    console.log('error in controller');
+  }
+  
+ }
+
+ 
 // whiskeyController.deleteWhiskeys = (req, res, next) => {
 // const id = await req.params;
 //   console.log('MIDDLEWARE deletewhiskey');
@@ -48,6 +79,7 @@ whiskeyController.getWhiskeys = (req, res, next) => {
 //     message: {err: 'Invalid delete'}
 //   }))
 // }
+
 
 
 
--- src/routes/whiskeyRouter.js
+++ src/routes/whiskeyRouter.js
@@ -7,11 +7,19 @@ const router = Router();
 //     console.log('using api route')
 // })
 
+//to get all data from elephant SQL
 router.get('/', whiskeyController.getWhiskeys, (req, res) => {
     return res.status(200).json(res.locals.whiskeys)
 });
 
+
+//to add row in table at elephan SQL
+router.post('/add', whiskeyController.addWhiskeys, (req, res) => {
+    return res.status(200).json(res.locals.addWhiskey);
+});
+
 // router.delete('/', whiskeyController.deleteWhiskey)
+
 
 
 
